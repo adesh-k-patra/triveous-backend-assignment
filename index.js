eea52ae729ca939fcb46f4d1e0c801e291ad4829
Adesh Kumar Patra <adeshkpatra@akpsmac-3.local>
2024-02-22 19:59:55 +0530

    Add Rate Limiter

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
-const mainRouter = require("./routes/mainRouter")
+const mainRouter = require('./routes/mainRouter')
+const { rateLimit } = require('express-rate-limit')
 const app = express()
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+  message: 'Too many requests from this IP, please try again later',
+});
+
+app.use(limiter)
 app.use(express.json())
 app.use("/api/vi", mainRouter)
 
